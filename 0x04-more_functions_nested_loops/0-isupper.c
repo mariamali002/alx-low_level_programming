e55f335037901b5609ad2340f1671c80b308bfb8
mariamali002 <mariaamali88@gmail.com>
2023-03-24 06:30:29 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,12 +3,12 @@
 /**
  * _isupper - uppercase letters
  * @c: char to check
- *
+ * Return: 1 for upper letter or 0 for any else
  * Return 0 or 1
  */
 int _isupper(int c)
 {
-	if(c >= 'A' && c <= 'Z')
+	if(c >= 65 && c <= 90)
 		return (1);
 	else 
 		return (0);
