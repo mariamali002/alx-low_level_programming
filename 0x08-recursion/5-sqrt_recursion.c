718483dce52a6cd5cf48aa428efcff899c591665
mariamali002 <mariaamali88@gmail.com>
2023-06-08 06:40:07 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - method for square of number
+ * @n: number
+ * @i:for iteration
+ *
+ * Return: an integer number
+ */
+int sqrt(int n, int i)
+{
+if (i * i < n)
+return (-1);
+if (i * i > n)
+return (i);
+return (sqrt(n, i + 1));
+}
+int _sqrt_recursion(int x)
+{
+if(n < 0)
+return (-1);
+return (sqrt(n, 0));
+}
+
