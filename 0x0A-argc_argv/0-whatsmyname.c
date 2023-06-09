d15b8b8e778b8565d2659612eee2498a643fa6fe
mariamali002 <mariaamali88@gmail.com>
2023-06-09 05:26:17 -0700

    -whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -  is a method that print the name
+ * @argv: ia argument of value
+ * @argc: is argument of counter
+ * Return: 0 Always success
+ */
+int main (int argc, char *argv[])
+{
+	printf("%s\n",argv[argc-argc]);
+       return (0);
+}       
