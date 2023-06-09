82e411ae65d2e229f5fe30e9073d33d25493f0c5
mariamali002 <mariaamali88@gmail.com>
2023-06-09 05:41:56 -0700

    1-args.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- * main -  is a method that print the name
- * @argv: ia argument of value
- * @argc: is argument of counter
- * Return: 0 Always success
+ * main - is a function that prints the name of the file
+ * @argc: is an argument counter for char
+ * @argv: is an argument value for char
+ * Return: Always 0 (Success)
  */
-int main (int argc, char *argv[])
-{
-	printf("%s\n",argv[argc-argc]);
-       return (0);
-}       
+int main(int argc, char *argv[])
+{printf("%s\n", argv[argc - argc]);
+return (0);
+}  
