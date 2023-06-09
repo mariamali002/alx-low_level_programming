87f3b02ee1e511420a78387db2b178cfee1bb5d0
mariamali002 <mariaamali88@gmail.com>
2023-06-09 05:15:49 -0700

    0x09-static_libraries

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ *  @c: input that is going to be checked
+ * Return: 1 if @c is uppercase, otherwise 0
+**/
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
