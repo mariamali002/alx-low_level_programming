6459423dc29501e065e361c1578910898cffa010
mariamali002 <mariaamali88@gmail.com>
2023-06-13 04:23:42 -0700

    1-strdup.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,27 +1,21 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * create_array - create and initialize an array to spacific char
- * @size: size 
+ * @size:size 
  * @c: char
  * Return: char pointer to malloc created memory address or NULL if error
  */
-
 char *create_array(unsigned int size, char c)
 {
 char *a;
 unsigned int i;
-
 if (size == 0)
 return (NULL);
-
 a = malloc(size * sizeof(*a));
 if (a == NULL)
 return (NULL);
-
 for (i = 0; i < size; i++)
 a[i] = c;
-
 return (a);
 }
