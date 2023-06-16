fbba3025932e3a412fd6fbb9996ff5da4f2ea070
mariamali002 <mariaamali88@gmail.com>
2023-06-16 08:43:40 -0700

    malloc free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,10 +10,10 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	int *ptr;
+int *ptr;
 
-	ptr = malloc(b);
-	if (ptr == NULL)
-		exit(98);
-	return (ptr);
+ptr = malloc(b);
+if (ptr == NULL)
+exit(98);
+return (ptr);
 }
